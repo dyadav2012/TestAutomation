da41e4ad4c8956bd13d48e0eaff86cef382564da
Dheeru <dyadav2012@gmail.com>
2019-06-13 14:21:10 +0530

    updated project

---
+++ src/main/java/com/dhairya/AutomationCucumber/App.java
@@ -0,0 +1,13 @@
+package com.dhairya.AutomationCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
